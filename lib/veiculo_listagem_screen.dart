3508c403d216e84927f762a211498a74a44c290a
ra4285 <ra4285@feitep.edu.br>
2025-11-11 23:41:25 +0000

    Atualiza 11/11/2025

--- lib/veiculo_listagem_screen.dart
+++ lib/veiculo_listagem_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/veiculo.dart';
+import 'package:myapp/veiculo_adicionar_screen.dart';
 
 class VeiculoListagemScreen extends StatefulWidget {
   const VeiculoListagemScreen({super.key});
@@ -9,75 +10,96 @@ class VeiculoListagemScreen extends StatefulWidget {
 }
 
 class _VeiculoListagemScreenState extends State<VeiculoListagemScreen> {
-List<Veiculo> veiculos = [
-  Veiculo(marca: "Toyota", modelo: "Corolla", ano: 2022),
-  Veiculo(marca: "Honda", modelo: "Civic", ano: 2022),
-  Veiculo(marca: "Subaru", modelo: "Impreza", ano: 2022),
-  Veiculo(marca: "Ford", modelo: "Focus", ano: 2022),
-  Veiculo(marca: "Chevrolet", modelo: "Camaro", ano: 2022),
-  Veiculo(marca: "Nissan", modelo: "Sentra", ano: 2022),
-  Veiculo(marca: "Hyundai", modelo: "Elantra", ano: 2022),
-  Veiculo(marca: "Kia", modelo: "Sportage", ano: 2022),
-  Veiculo(marca: "Mazda", modelo: "CX-5", ano: 2022),
-  Veiculo(marca: "Tesla", modelo: "Model S", ano: 2022),
-  Veiculo(marca: "BMW", modelo: "X5", ano: 2022),
-  Veiculo(marca: "Mercedes-Benz", modelo: "C-Class", ano: 2022),
-  Veiculo(marca: "Audi", modelo: "A4", ano: 2022),
-  Veiculo(marca: "Volkswagen", modelo: "Golf", ano: 2022),
-  Veiculo(marca: "Toyota", modelo: "Corolla", ano: 2022),
-  Veiculo(marca: "Honda", modelo: "Civic", ano: 2022),
-  Veiculo(marca: "Subaru", modelo: "Impreza", ano: 2022),
-  Veiculo(marca: "Ford", modelo: "Focus", ano: 2022),
-  Veiculo(marca: "Chevrolet", modelo: "Camaro", ano: 2022),
-  Veiculo(marca: "Nissan", modelo: "Sentra", ano: 2022),
-  Veiculo(marca: "Hyundai", modelo: "Elantra", ano: 2022),
-  Veiculo(marca: "Kia", modelo: "Sportage", ano: 2022),
-  Veiculo(marca: "Mazda", modelo: "CX-5", ano: 2022),
-  Veiculo(marca: "Tesla", modelo: "Model S", ano: 2022),
-  Veiculo(marca: "BMW", modelo: "X5", ano: 2022),
-  Veiculo(marca: "Mercedes-Benz", modelo: "C-Class", ano: 2022),
-  Veiculo(marca: "Audi", modelo: "A4", ano: 2022),
-  Veiculo(marca: "Volkswagen", modelo: "Golf", ano: 2022),
-];
+  List<Veiculo> veiculos = [
+    Veiculo(marca: "Toyota", modelo: "Corolla", ano: 2022),
+    Veiculo(marca: "Honda", modelo: "Civic", ano: 2022),
+    Veiculo(marca: "Subaru", modelo: "Impreza", ano: 2022),
+    Veiculo(marca: "Ford", modelo: "Focus", ano: 2022),
+    Veiculo(marca: "Chevrolet", modelo: "Camaro", ano: 2022),
+    Veiculo(marca: "Nissan", modelo: "Sentra", ano: 2022),
+    Veiculo(marca: "Hyundai", modelo: "Elantra", ano: 2022),
+    Veiculo(marca: "Kia", modelo: "Sportage", ano: 2022),
+    Veiculo(marca: "Mazda", modelo: "CX-5", ano: 2022),
+    Veiculo(marca: "Tesla", modelo: "Model S", ano: 2022),
+    Veiculo(marca: "BMW", modelo: "X5", ano: 2022),
+    Veiculo(marca: "Mercedes-Benz", modelo: "C-Class", ano: 2022),
+    Veiculo(marca: "Audi", modelo: "A4", ano: 2022),
+    Veiculo(marca: "Volkswagen", modelo: "Golf", ano: 2022),
+    Veiculo(marca: "Toyota", modelo: "Corolla", ano: 2022),
+    Veiculo(marca: "Honda", modelo: "Civic", ano: 2022),
+    Veiculo(marca: "Subaru", modelo: "Impreza", ano: 2022),
+    Veiculo(marca: "Ford", modelo: "Focus", ano: 2022),
+    Veiculo(marca: "Chevrolet", modelo: "Camaro", ano: 2022),
+    Veiculo(marca: "Nissan", modelo: "Sentra", ano: 2022),
+    Veiculo(marca: "Hyundai", modelo: "Elantra", ano: 2022),
+    Veiculo(marca: "Kia", modelo: "Sportage", ano: 2022),
+    Veiculo(marca: "Mazda", modelo: "CX-5", ano: 2022),
+    Veiculo(marca: "Tesla", modelo: "Model S", ano: 2022),
+    Veiculo(marca: "BMW", modelo: "X5", ano: 2022),
+    Veiculo(marca: "Mercedes-Benz", modelo: "C-Class", ano: 2022),
+    Veiculo(marca: "Audi", modelo: "A4", ano: 2022),
+    Veiculo(marca: "Volkswagen", modelo: "Golf", ano: 2022),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Listagem de veículos", style: TextStyle(fontSize: 30),),
+        title: Text("Listagem de veículos", style: TextStyle(fontSize: 30)),
         backgroundColor: Colors.blueAccent,
         foregroundColor: Colors.white,
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
-          SizedBox(height: 10,),
-          FloatingActionButton(onPressed: (){}, child: Icon(Icons.abc_outlined),),
-          FloatingActionButton(onPressed: (){}, child: Icon(Icons.access_alarm),)
+          FloatingActionButton(
+            heroTag: "veiculo.add",
+            onPressed: () async {
+              Veiculo  resultado = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => VeiculoAdicionarScreen(),
+                ),
+              );
+
+              if (resultado != null) {
+                setState(() {
+                  veiculos.add(resultado);
+                });
+              }
+    
+            },
+            child: Icon(Icons.add),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.abc_outlined),
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.access_alarm),
+          ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: veiculos.map<Widget>((veiculo) => construirItemVeiculo(veiculo)).toList(),
+          children: veiculos
+              .map<Widget>((veiculo) => construirItemVeiculo(veiculo))
+              .toList(),
         ),
       ),
     );
   }
 
-  Widget construirItemVeiculo(Veiculo veiculo){
+  Widget construirItemVeiculo(Veiculo veiculo) {
     return Container(
       height: 50,
       width: double.infinity,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black, width: 2),
       ),
-      child: Column(
-        children: [
-          Text("Modelo: "),
-          Text(veiculo.modelo!)
-        ],
-      ),
+      child: Column(children: [Text("Modelo: "), Text(veiculo.modelo!)]),
     );
   }
 }
